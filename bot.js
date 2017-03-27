66c8342d06ce6d3efed866fb0fefe9281ba17a8d
shubhamjuneja11 <shubhamjuneja11@gmail.com>
2017-03-27 19:56:01 +0530

    bot working

--- bot.js
+++ bot.js
@@ -2,3 +2,12 @@ console.log('Bot Started');
 var Twit = require('twit')
 var config=require('./config');
 var T = new Twit(config);
+
+var params={
+ q: 'kapil sharma', 
+ count:3
+}
+T.get('search/tweets',params, function(err, data, response) {
+  console.log(data)
+})
+
